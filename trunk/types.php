68908c8fade5c24314f1cd62282c3d5679186c66
shimniok@gmail.com <shimniok@gmail.com@bd15caba-f2e2-cba1-1b2e-bb6af8f81b88>
2013-09-13 14:58:37 +0000

    Revised php files, generified data, table drawing, etc.

--- trunk/types.php
+++ trunk/types.php
@@ -1,59 +1,22 @@
+<script type='text/javascript'>actionUrl="type.action.php";</script>
 <?php
-include_once "common/base.php";
-$pageTitle="Types";
-include_once "common/header.php"; 
+/**
+ * types.php - display list of types, editable
+ * 
+ * @author Michael Shimniok
+ */
+include_once 'common/base.php';
+$pageTitle='Types';
+include_once 'common/header.php';
+include_once 'inc/data.inc.php';
+
+$data = array( 'main' => $type->load() );
+$metadata = array( 'name' => array('title' => 'Name', 'edit' => true) );
+
+echo "<div id=\"main\">";
+renderTable2($metadata, $data); // render the whole page
+echo "</div>";
+
+include_once "common/menu.php";
+include_once "common/footer.php"; 
 ?>
-
-<div id="main">
-
-	<noscript>This site just doesn't work, period, without JavaScript</noscript>
-
-<?php
-	// List types
-	include_once 'inc/class.types.inc.php';
-    $list = new Types($db);
-    
-    if (isset($_GET['mode'])) {
-		if ($_GET['mode'] == "add") {
-			$list->add($_GET['name']);
-		} else if ($_GET['mode'] == "delete") {
-			$list->del($_GET['id']);
-		}
-	}
-	
-    $entries = $list->load();
-?>
-	<form action="" id="add-new" method="GET"> 
-	<input type="hidden" name="mode" value="add">
-	<table class='bordered'>
-		<thead>
-		<tr>
-			<th>Name</th>
-			<th class='centered'>Action</th>
-		</tr>
-		</thead>
-		<tbody>
-<?php
-	foreach ($entries as $entry) {
-		echo "<tr>\n";
-		echo "<td>".$entry['name']."</td>";
-		echo "<td>";
-		echo "<input type='button' value='Edit' />";
-		echo "<input type='button' value='Del' />";
-		echo "</td>\n";
-		echo "</tr>";
-	}
-?>
-		</tbody>
-		<tfoot>
-		<tr>
-			<td><input type="text" name="name" size="20"></td>
-			<td class='centered'><input type="submit" id="add-new-submit" value="Add" class="button" /></td>
-		</tr>
-		</tfoot>
-	</table>
-	</form>
-</div>
-
-<?php include_once "common/menu.php"; ?>
-<?php include_once "common/footer.php"; ?>
