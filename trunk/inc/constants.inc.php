e6804c378d1ea0f8dd1ad61b727621872aa8a148
shimniok@gmail.com <shimniok@gmail.com@bd15caba-f2e2-cba1-1b2e-bb6af8f81b88>
2013-09-17 15:13:52 +0000

    constants change

--- trunk/inc/constants.inc.php
+++ trunk/inc/constants.inc.php
@@ -2,7 +2,7 @@
     // Database credentials
     define('DB_HOST', 'localhost');
     define('DB_USER', 'test');
-    define('DB_PASS', 'supra29');
+    define('DB_PASS', '7qgACqPaqgjq');
     define('DB_NAME', 'test');
     define('OCTOPART_API_KEY', '6bca5c56');
 ?>
