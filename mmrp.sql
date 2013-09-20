8f8bf262c1bbd3206fc85170affa9f12b25f50fa
shimniok@gmail.com <shimniok@gmail.com@bd15caba-f2e2-cba1-1b2e-bb6af8f81b88>
2013-09-20 16:05:26 +0000

    Modified sql create script, drops before creating, and doesn't specify schema

--- mmrp.sql
+++ mmrp.sql
@@ -2,170 +2,196 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 ;
-USE `test` ;
 
 -- -----------------------------------------------------
--- Table `test`.`types`
+-- Table `subtypes`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`types` (
+DROP TABLE IF EXISTS `subtypes` ;
+
+CREATE  TABLE IF NOT EXISTS `subtypes` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL ,
+  `name` VARCHAR(45) NULL COMMENT 'subtype name' ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test`.`subtypes`
+-- Table `types`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`subtypes` (
+DROP TABLE IF EXISTS `types` ;
+
+CREATE  TABLE IF NOT EXISTS `types` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL ,
+  `name` VARCHAR(45) NULL COMMENT 'type name' ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test`.`parts`
+-- Table `parts`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`parts` (
+DROP TABLE IF EXISTS `parts` ;
+
+CREATE  TABLE IF NOT EXISTS `parts` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `inventory` INT NULL DEFAULT 0 COMMENT 'current inventory' ,
-  `ordered` INT NULL DEFAULT 0 ,
-  `partNo` VARCHAR(45) NULL DEFAULT NULL ,
-  `footprint` VARCHAR(45) NULL DEFAULT NULL ,
-  `value` VARCHAR(45) NULL DEFAULT NULL ,
-  `voltage` VARCHAR(45) NULL DEFAULT NULL ,
-  `tolerance` VARCHAR(45) NULL DEFAULT NULL ,
-  `types_id` INT NOT NULL ,
-  `subtypes_id` INT NOT NULL ,
-  `partscol` VARCHAR(45) NULL ,
+  `inventory` INT(11) NULL DEFAULT '0' COMMENT 'quantity in inventory' ,
+  `ordered` INT(11) NULL DEFAULT '0' COMMENT 'quantity on order' ,
+  `partNo` VARCHAR(45) NULL DEFAULT NULL COMMENT 'supplier part number' ,
+  `footprint` VARCHAR(45) NULL DEFAULT NULL COMMENT 'part footprint' ,
+  `value` VARCHAR(45) NULL DEFAULT NULL COMMENT 'value associated with part (passives,)' ,
+  `voltage` VARCHAR(45) NULL DEFAULT NULL COMMENT 'voltage rating' ,
+  `tolerance` VARCHAR(45) NULL DEFAULT NULL COMMENT 'tolerance (passives)' ,
+  `types_id` INT(11) NOT NULL COMMENT 'part type: types key' ,
+  `subtypes_id` INT(11) NOT NULL COMMENT 'part subtype: subtypes key' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_parts_types` (`types_id` ASC) ,
   INDEX `fk_parts_subtype1` (`subtypes_id` ASC) ,
-  CONSTRAINT `fk_parts_types`
-    FOREIGN KEY (`types_id` )
-    REFERENCES `test`.`types` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_parts_subtype1`
     FOREIGN KEY (`subtypes_id` )
-    REFERENCES `test`.`subtypes` (`id` )
+    REFERENCES `subtypes` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_parts_types`
+    FOREIGN KEY (`types_id` )
+    REFERENCES `types` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-AUTO_INCREMENT = 7
+AUTO_INCREMENT = 122
 DEFAULT CHARACTER SET = latin1
 COLLATE = latin1_swedish_ci;
 
 
 -- -----------------------------------------------------
--- Table `test`.`products`
+-- Table `products`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`products` (
+DROP TABLE IF EXISTS `products` ;
+
+CREATE  TABLE IF NOT EXISTS `products` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL DEFAULT NULL ,
-  `inventory` INT NULL DEFAULT 0 ,
-  `needed` INT NULL DEFAULT 0 COMMENT 'number of products\nneeded to be built' ,
+  `name` VARCHAR(45) NULL DEFAULT NULL COMMENT 'product name' ,
+  `inventory` INT NULL DEFAULT 0 COMMENT 'quantity in inventory' ,
+  `needed` INT NULL DEFAULT 0 COMMENT 'number of products\nto be built' ,
   `sold` INT NULL DEFAULT 0 COMMENT 'number of product\nsold' ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test`.`dataface__mtimes`
+-- Table `product_part`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`dataface__mtimes` (
-  `name` VARCHAR(255) NOT NULL ,
-  `mtime` INT(11) NULL DEFAULT NULL ,
-  PRIMARY KEY (`name`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+DROP TABLE IF EXISTS `product_part` ;
 
-
--- -----------------------------------------------------
--- Table `test`.`product_part`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`product_part` (
+CREATE  TABLE IF NOT EXISTS `product_part` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT 'unique bom id' ,
   `products_id` INT NOT NULL COMMENT 'products key' ,
   `parts_id` INT(11) NOT NULL COMMENT 'parts key' ,
   `qty` INT NULL DEFAULT 0 COMMENT 'quantity used per product' ,
-  `needed` INT NULL DEFAULT 0 COMMENT 'number needed for this particular products_id' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_products_has_parts_parts1` (`parts_id` ASC) ,
   INDEX `fk_products_has_parts_products1` (`products_id` ASC) ,
   CONSTRAINT `fk_products_has_parts_products1`
     FOREIGN KEY (`products_id` )
-    REFERENCES `test`.`products` (`id` )
+    REFERENCES `products` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_products_has_parts_parts1`
     FOREIGN KEY (`parts_id` )
-    REFERENCES `test`.`parts` (`id` )
+    REFERENCES `parts` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `test`.`build_history`
+-- Table `build_history`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `test`.`build_history` (
-  `id` INT NOT NULL ,
-  `qty` INT NULL ,
-  `products_id` INT NOT NULL ,
+DROP TABLE IF EXISTS `build_history` ;
+
+CREATE  TABLE IF NOT EXISTS `build_history` (
+  `id` INT(11) NOT NULL ,
+  `qty` INT(11) NULL DEFAULT NULL COMMENT 'number of product built' ,
+  `products_id` INT(11) NOT NULL COMMENT 'product built' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_build_history_products1` (`products_id` ASC) ,
   CONSTRAINT `fk_build_history_products1`
     FOREIGN KEY (`products_id` )
-    REFERENCES `test`.`products` (`id` )
+    REFERENCES `products` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci
+COMMENT = 'build_hour';
 
 
 -- -----------------------------------------------------
--- Placeholder table for view `test`.`partcount`
+-- Placeholder table for view `partcount`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test`.`partcount` (`id` INT, `total` INT, `available` INT);
+CREATE TABLE IF NOT EXISTS `partcount` (`id` INT, `total` INT, `available` INT);
 
 -- -----------------------------------------------------
--- Placeholder table for view `test`.`builds`
+-- Placeholder table for view `builds`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `test`.`builds` (`parts_id` INT, `total` INT);
+CREATE TABLE IF NOT EXISTS `builds` (`parts_id` INT, `total` INT);
 
 -- -----------------------------------------------------
--- View `test`.`partcount`
+-- procedure build_one
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `test`.`partcount`;
-USE `test`;
-CREATE  OR REPLACE VIEW `test`.`partcount` AS
-    SELECT parts.id, 
-        sum(product_part.qty * products.needed) AS total,
-        parts.inventory
-            + parts.ordered
-            - sum(product_part.qty * products.needed) AS available
-    FROM parts 
-    LEFT JOIN product_part ON parts.id=product_part.parts_id 
-    LEFT JOIN products ON products.id=product_part.products_id 
-    GROUP BY parts.id;
-;
+DROP procedure IF EXISTS `build_one`;
+CREATE PROCEDURE `build_one` (prod_id INT)
+BEGIN
+    -- update products needed and inventory, parts inventory
+    update product_part
+        inner join products on product_part.products_id = products.id
+        inner join parts on product_part.parts_id = parts.id
+        set
+            parts.inventory=parts.inventory-product_part.qty, -- update parts inventory based on qty
+            products.needed=products.needed-1,                -- decrement product needed
+            products.inventory=products.inventory+1           -- increment product inventory
+        where product_part.products_id=prod_id;
+    -- add/update an entry to build_history
+    insert into build_history (id, qty, products_id) 
+        values(date_format(now(),'%Y%m%d%H'), 1, prod_id)     -- bucket builds per hour/day/month/year
+        on duplicate key update qty=qty+values(qty);
+END 
+$$
 
 -- -----------------------------------------------------
--- View `test`.`builds`
+-- procedure unbuild_one
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `test`.`builds`;
-USE `test`;
-CREATE  OR REPLACE VIEW `test`.`builds` AS
-    SELECT 
-        product_part.parts_id, 
-        sum(product_part.qty * build_history.qty) AS total
-    FROM product_part 
-    JOIN build_history 
-        ON product_part.products_id = build_history.products_id
-    GROUP BY parts_id;
-;
+DROP procedure IF EXISTS `unbuild_one`;
+
+
+CREATE PROCEDURE `unbuild_one` (build_id INT)
+BEGIN
+    -- update products needed and inventory, parts inventory, build_history
+    update product_part
+        inner join build_history on product_part.products_id=build_history.products_id
+        inner join products on product_part.products_id = products.id
+        inner join parts on product_part.parts_id = parts.id
+        set
+            parts.inventory=parts.inventory+product_part.qty, -- update parts inventory based on qty
+            products.needed=products.needed+1,                -- decrement product needed
+            products.inventory=products.inventory-1,          -- increment product inventory
+            build_history.qty=build_history.qty-1
+        where build_history.id = build_id and build_history.qty > 0;
+END 
+$$
+
+-- -----------------------------------------------------
+-- View `partcount`
+-- -----------------------------------------------------
+DROP VIEW IF EXISTS `partcount` ;
+DROP TABLE IF EXISTS `partcount`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `partcount` AS select `parts`.`id` AS `id`,sum((`product_part`.`qty` * `products`.`needed`)) AS `total`,((`parts`.`inventory` + `parts`.`ordered`) - sum((`product_part`.`qty` * `products`.`needed`))) AS `available` from ((`parts` left join `product_part` on((`parts`.`id` = `product_part`.`parts_id`))) left join `products` on((`products`.`id` = `product_part`.`products_id`))) group by `parts`.`id`;
+
+-- -----------------------------------------------------
+-- View `builds`
+-- -----------------------------------------------------
+DROP VIEW IF EXISTS `builds` ;
+DROP TABLE IF EXISTS `builds`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `builds` AS select `product_part`.`parts_id` AS `parts_id`,sum((`product_part`.`qty` * `build_history`.`qty`)) AS `total` from (`product_part` join `build_history` on((`product_part`.`products_id` = `build_history`.`products_id`))) group by `product_part`.`parts_id`;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
