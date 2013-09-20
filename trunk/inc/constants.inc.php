d54846bd06e3fb563bc539a9c0c1bd29ae087a3b
shimniok@gmail.com <shimniok@gmail.com@bd15caba-f2e2-cba1-1b2e-bb6af8f81b88>
2013-09-20 16:07:44 +0000

    changed db name back to normal

--- trunk/inc/constants.inc.php
+++ trunk/inc/constants.inc.php
@@ -3,6 +3,6 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'test');
     define('DB_PASS', '7qgACqPaqgjq');
-    define('DB_NAME', 'test2');
+    define('DB_NAME', 'test');
     define('OCTOPART_API_KEY', '6bca5c56');
 ?>
