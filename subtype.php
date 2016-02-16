6b329afb147041e424e31075e436751407402cbb
shimniok@gmail.com <shimniok@gmail.com>
2016-02-16 16:37:54 -0700

    Got dynamic addition working with REST call

--- subtype.php
+++ subtype.php
@@ -12,14 +12,18 @@ switch($_GET['mode']) {
 	case 'list' :
 	  echo json_encode($subtype->load());
 		break;
+		/*
 	case 'update' :
 		$id = $_GET['id'];
 		$data = array('name' => $_GET['name'] );
 		$subtype->update($id, $data);
 		break;
+		*/
 	case 'add' :
 		$data = array('name' => $_GET['name'] );
 		$id = $subtype->add($data);
+		$data['id'] = $id;
+		echo json_encode($data);
 		break;
 	case 'delete' :
 		$id = $_GET['id'];
