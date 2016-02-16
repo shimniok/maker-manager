8adb7c4f9ee2754cb9b1f0f82cb7b35cace83022
shimniok@gmail.com <shimniok@gmail.com>
2016-02-16 13:02:38 -0700

    modded to use new api URL

--- mmrp.js
+++ mmrp.js
@@ -1,8 +1,20 @@
 
 var app = angular.module('mmrpApp', []);
 
+app.controller('ProductController', function($scope, $http){
+  $scope.bom = null;
+  $http.get('api/product/list').then(function(resp){
+    console.log('Success', resp);
+    // For JSON responses, resp.data contains the result
+    $scope.products = resp.data;
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  });
+});
+
 app.controller('PartController', function($scope, $http) {
-  $http.get('part.php?mode=list').then(function(resp) {
+  $http.get('api/part/list').then(function(resp) {
     console.log('Success', resp);
     // For JSON responses, resp.data contains the result
     $scope.parts = resp.data;
@@ -13,10 +25,21 @@ app.controller('PartController', function($scope, $http) {
 });
 
 app.controller('TypeController', function($scope, $http) {
-  $http.get('type.php?mode=list').then(function(resp) {
+  $http.get('api/type/list').then(function(resp) {
     console.log('Success', resp);
     // For JSON responses, resp.data contains the result
     $scope.types = resp.data;
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  });
+});
+
+app.controller('SubtypeController', function($scope, $http) {
+  $http.get('api/subtype/list').then(function(resp) {
+    console.log('Success', resp);
+    // For JSON responses, resp.data contains the result
+    $scope.subtypes = resp.data;
   }, function(err) {
     console.error('ERR', err);
     // err.status will contain the status code
