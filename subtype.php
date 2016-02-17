fce7cf6e30819b325c5076c485a0f02fbce197c7
shimniok@gmail.com <shimniok@gmail.com>
2016-02-17 11:44:10 -0700

    modded for standard rest interface using standard http methods, and path-based arguments

--- subtype.php
+++ subtype.php
@@ -1,34 +1,72 @@
 <?php
 /**
- * action script for ajax submit of Parts
+ * REST interface
+ *
+ * Expects to be called with the following pattern:
+ * GET BASE_URL/thingy       - Retrieves a list of thingys
+ * GET BASE_URL/thingy/12    - Retrieves a specific thingy (#12)
+ * POST BASE_URL/thingy      - Creates a new thingy
+ * PUT BASE_URL/thingy/12    - Updates thingy #12
+ * DELETE BASE_URL/thingy/12 - Deletes thingy #12
  */
 include_once 'common/base.php';
 
-$id = "";
+// Get the API name based on the filename
+$me = preg_replace('/\.php$/', '', basename(__FILE__));
 
+// Determine the arguments in the URI
+$path = explode("$me/", $_SERVER['REQUEST_URI'] );
+$argv = array();
+if (isset($path[1])) {
+	$argv = explode( ',', $path[1]);
+}
+$argc = count($argv);
+
+if ($argc > 1) {
+	exit(2);
+}
+
+// Prep the data access object
 $subtype = new Data($db, 'subtypes', 'id', array('name', 'id'));
 
-switch($_GET['mode']) {
-	case 'list' :
-	  echo json_encode($subtype->load());
+switch($_SERVER['REQUEST_METHOD']){
+	case 'GET':
+		if ($argc == 1) {
+			// Retrieve one
+			echo json_encode($subtype->loadRow('id', $argv[0]));
+		} else {
+			// Retrieve all
+			echo json_encode($subtype->load());
+		}
 		break;
-		/*
-	case 'update' :
-		$id = $_GET['id'];
-		$data = array('name' => $_GET['name'] );
-		$subtype->update($id, $data);
+	case 'POST':
+		// Create new
+		if (isset($_POST['name'])) {
+			$id = $subtype->add($_POST['name']);
+			echo json_encode(array('name' => $_POST['name'], 'id' => $id));
+		} else {
+			echo json_encode(array());
+		}
 		break;
-		*/
-	case 'add' :
-		$data = array('name' => $_GET['name'] );
-		$id = $subtype->add($data);
-		$data['id'] = $id;
-		echo json_encode($data);
+	case 'DELETE':
+		// Delete one
+		if ($argc == 1) {
+			$id = $argv[0];
+			$id = $subtype->del($id);
+			echo json_encode($id);
+		} else {
+			echo json_encode(array());
+		}
 		break;
-	case 'delete' :
-		$id = $_GET['id'];
-		$subtype->del($_GET['id']);
-		echo json_encode($id);
+	case 'PUT':
+		if (argc == 1 && isset($_POST['name'])) {
+			$id = $argv[0];
+			$data = array('name' => $_POST['name']);
+			$id = $subtype->update($id, $data);
+			echo json_encode($id);
+		} else {
+			echo json_encode(array());
+		}
 		break;
 }
 
