98153a835641946a8b758ea020804905311a516e
shimniok@gmail.com <shimniok@gmail.com>
2016-02-17 16:13:54 -0700

    Moved handy globals and writeLog function in. Changed dependency on data class php

--- common/base.php
+++ common/base.php
@@ -1,7 +1,12 @@
 <?php
 
 /**
- * Sets up the PHP session and database connection
+ * Sets up the PHP session and database connection provides log function
+ * and sets error reporting level, and defines global variables:
+ *
+ *  $me   - basename of the current php file (used for REST API)
+ *  $argv - array of arguments extracted from URI (REST API)
+ *  $argc - count of URI arguments
  *
  * PHP version 5
  *
@@ -29,5 +34,27 @@ try {
 	exit;
 }
 
-include_once 'inc/class.data.inc.php';
+// Get the API name based on the current php filename
+$me = preg_replace('/\.php$/', '', basename(__FILE__));
+
+// Extract any arguments from the URI
+$argv = preg_split('/\//',
+	preg_replace( '/^.*\/'.$me.'\/{0,1}/', '', $_SERVER['REQUEST_URI']),-1, PREG_SPLIT_NO_EMPTY);
+$argc = count($argv);
+
+/**
+ * writeLog writes the provided message to the logfile along with date and some other info.
+ * Expects a global variables $me and constant LOG_FILE
+ *
+ * @param constant LOG_FILE - filename of log file
+ * @param string $me        - php module identifier
+ * @param string $msg       - the message to write to the logfile
+ */
+function writeLog($msg) {
+	global $me;
+	$date = date("m/d/Y h:i:s A (T)");
+	error_log("$date $me $msg\n", 3, LOG_FILE);
+}
+
+include_once 'class.data.php';
 ?>
