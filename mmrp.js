9ccae51b401ce5de5ce179dad554799e17df2ba4
shimniok@gmail.com <shimniok@gmail.com>
2016-02-17 17:29:11 -0700

    modified rest api paths

--- mmrp.js
+++ mmrp.js
@@ -3,7 +3,7 @@ var app = angular.module('mmrpApp', []);
 
 app.controller('ProductController', function($scope, $http){
   $scope.bom = null;
-  $http.get('api/product/list').then(function(resp){
+  $http.get('api/product').then(function(resp){
     console.log('Success', resp);
     // For JSON responses, resp.data contains the result
     $scope.products = resp.data;
@@ -14,7 +14,7 @@ app.controller('ProductController', function($scope, $http){
 });
 
 app.controller('PartController', function($scope, $http) {
-  $http.get('api/part/list').then(function(resp) {
+  $http.get('api/part').then(function(resp) {
     console.log('Success', resp);
     // For JSON responses, resp.data contains the result
     $scope.parts = resp.data;
