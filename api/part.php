a85744e1b9df3aceaa5465327864ef16ee9c0c62
shimniok@gmail.com <shimniok@gmail.com>
2016-02-19 18:58:37 -0700

    made generic rest handler class, converted part.php to use it

--- api/part.php
+++ api/part.php
@@ -2,42 +2,15 @@
 /**
  * action script for ajax submit of Parts
  */
-include_once 'common/base.php';
+include_once 'base.php';
+require 'class.restful.php';
 
-$part = new Data($db, 'parts', 'id',
-				 array('id', 'inventory', 'ordered', 'partNo', 'footprint',
-					   'value', 'voltage', 'tolerance', 'types_id', 'subtypes_id'));
 
-// input validation?
-$id = "";
-if (isset($_POST['id'])) {
-	$id = $_POST['id'];
-}
 
-switch($_GET['mode']) {
-	case 'list' :
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		// headers to tell that result is JSON
-		header('Content-type: application/json');
+$partData = new Data($db, 'parts', 'id', array('id', 'inventory', 'ordered', 'partNo', 'footprint',	'value', 'voltage', 'tolerance', 'types_id', 'subtypes_id'));
 
-		echo json_encode($part->load());
-		break;
-	case 'update' :
-		$part->update($id, $data);
-		break;
-	case 'add' :
-		$id = $part->add($data);
-		break;
-	case 'received' :
-		$row = $part->loadRow('id', $id);
-		$data['inventory'] = $row['inventory'] + $row['ordered'];
-		$data['ordered'] = $row['ordered'] = 0;
-		$part->update($id, $data);
-		break;
-	case 'delete' :
-		$part->del($id);
-		break;
-}
+$rest = new Restful("part");
+
+$rest->handleRequest($partData);
 
 ?>
