ecaf688d9b8f52b9d1e820fed043c907f399f007
shimniok@gmail.com <shimniok@gmail.com>
2016-03-09 13:03:14 -0700

    Got edit working with passed-in form name.

--- js/directives/mmrp-edit.js
+++ js/directives/mmrp-edit.js
@@ -2,6 +2,9 @@ angular.module('MakerMRP')
 .directive('mmrpEdit', function(){
   return {
     restrict: 'E',
-    templateUrl: 'templates/edit.html'
-  }
+    templateUrl: 'templates/edit.html',
+    scope: {
+      form: '='
+    }
+  };
 });
