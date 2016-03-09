3147b44150464b6cc56e81e4a245d39c60c904dd
shimniok@gmail.com <shimniok@gmail.com>
2016-03-09 08:16:08 -0700

    Added route for post, removed unused vars, use statements. trailing ?>, added json pre-processing

--- api/products.php
+++ api/products.php
@@ -10,23 +10,38 @@
  * DELETE BASE_URL/thingy/12 - Deletes thingy #12
  */
 include_once 'base.php';
-require_once __DIR__.'/silex/vendor/autoload.php';
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+require_once __DIR__ . '/silex/vendor/autoload.php';
 
-$app = new Silex\Application();
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+
+$app = new Application();
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
 $columns = array('id', 'inventory', 'needed', 'name');
 $db = new Data($db, 'products', 'id', $columns);
 
-$app->get('/products', function() use($db, $columns) {
-  return json_encode($db->query());
+$app->get('/products', function() use($db) {
+    return json_encode($db->query());
 });
 
-$app->get('/products/{id}', function (Silex\Application $app, $id) use ($db, $columns) {
-  return json_encode($db->query('id', $id, 1));
+$app->get('/products/{id}', function ($id) use ($db) {
+    return json_encode($db->query('id', $id, 1));
+});
+
+$app->post('/products', function(Request $request) use($db) {
+    $new = array(
+        "name" => $request->get("name"),
+        "needed" => $request->get("needed"),
+        "inventory" => $request->get("inventory")
+    );
+    return json_encode($db->add($new));
 });
 
 $app->run();
-
-
-?>
