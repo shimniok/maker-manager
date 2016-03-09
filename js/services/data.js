0e1760dc6cc6bd9c1d1aba1dd6ac400156fd845e
shimniok@gmail.com <shimniok@gmail.com>
2016-03-08 17:55:01 -0700

    Added comments. Updated template to translate type/subtype ids into names.

--- js/services/data.js
+++ js/services/data.js
@@ -5,17 +5,30 @@ angular.module('MakerMRP')
   //
   .factory('DataService', function ($q, $filter, ProductsResource, PartsResource, TypesResource, SubtypesResource, BomsResource) {
     var me = this;
-    // Preload data
+
+    // Pre-load data to ensure speedy-quick response. The resources return a promise that eventually gets resolved.
+    // Unfortunately for us, some other properties depend on these. See currentProduct, CurrentBom below.
+    //
     this.parts = PartsResource.query();
     this.types = TypesResource.query();
     this.subtypes = SubtypesResource.query();
     this.boms = BomsResource.query();
     this.products = ProductsResource.query();
     this.currentProductId = null;
+
+    // Here we're setting the currentProduct to the promise from products so that
+    // when resolved, the currentProduct is updated. This ensures that upon browser
+    // refresh, the current product is eventually resolved.
+    //
     this.currentProduct = this.products.$promise.then(function(result) {
       console.log("currentProduct: then()", me.currentProduct);
       me.updateCurrentProduct();
     });
+
+    // Here we're setting currentBom to a promise, chained to its dependencies,
+    // currentProduct, boms, and parts. Once those resolve, we update the current
+    // bom. This ensures that upon browser refresh, the current Bom is eventually resolved.
+    //
     this.currentBom = this.currentProduct.then(function(result) {
       me.boms.$promise.then(function(result){
         me.parts.$promise.then(function(result){
@@ -25,10 +38,22 @@ angular.module('MakerMRP')
       });
     });
 
+    /**
+     * Get the name of the specified type
+     *
+     * @param id is the id of the type
+     * @returns {string} the name of the specified type
+     */
     this.getTypeName = function(id) {
       return (id in this.types) ? this.types[id].name : "?";
     };
 
+    /**
+     * * Get the name of the specified subtype
+     *
+     * @param id is the id of the subtype
+     * @returns {string} the name of the specified subtype
+     */
     this.getSubtypeName = function(id) {
       return (id in this.subtypes) ? this.subtypes[id].name : "?";
     };
@@ -62,6 +87,9 @@ angular.module('MakerMRP')
       return $filter('filter')(this.boms, {products_id: id});
     }
 
+    /**
+     * Updates the current bom list based on the currentProductId
+     */
     this.updateCurrentBom = function() {
       if (me.currentProductId != null) {
         var bom = $filter('filter')(this.boms, { products_id: this.currentProductId });
@@ -73,6 +101,9 @@ angular.module('MakerMRP')
       }
     }
 
+    /**
+     * Updates the current product object based on currentProductId
+     */
     this.updateCurrentProduct = function() {
       if (me.currentProductId != null) {
         console.log("updateCurrentProduct() id:", me.currentProductId);
@@ -81,6 +112,12 @@ angular.module('MakerMRP')
       }
     }
 
+    /**
+     * Called by a controller to set the currentProductId and subsequently make calls to
+     * update current Product and Bom.
+     *
+     * @param id is the specified product to assign to currentProduct and to use for currentBom
+     */
     this.setCurrentProduct = function(id) {
       this.currentProductId = id;
       this.updateCurrentProduct();
