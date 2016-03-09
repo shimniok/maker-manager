f23775b6b0beac5cf37a9932e2d8707a99a2491b
shimniok@gmail.com <shimniok@gmail.com>
2016-03-09 08:07:54 -0700

    Added add() method, wired up to subtypes resource

--- js/controllers/subtypes.js
+++ js/controllers/subtypes.js
@@ -1,28 +1,28 @@
 angular.module('MakerMRP')
-//////////////////////////////////////////////////////////////////////
-// SUBYTPE Controller
-//
-.controller('SubtypeController', function(DataService, $scope) {
-  $scope.data = DataService;
-  $scope.newSubtype = {id: 0, name: ""};
+  //////////////////////////////////////////////////////////////////////
+  // SUBYTPE Controller
+  //
+  .controller('SubtypeController', function (DataService, SubtypesResource, $scope) {
+    $scope.data = DataService;
+    $scope.new = new SubtypesResource();
 
-  /*
-  $scope.add = function(){
-    console.log('add(): ', $scope.newSubtype);
-    $http.post('api/subtype', $scope.newSubtype ).then(function(resp){
-      console.log('Success', resp.data);
-      $scope.subtypes[resp.data['id']] = resp.data;
-      $scope.newSubtype = {};
-    });
-  };
+    $scope.add = function () {
+      console.log('SubtypeController.add(): ', $scope.new);
+      $scope.new.$save(function (subtype) {
+        DataService.subtypes.push(subtype);
+        $scope.new = new SubtypesResource();
+        console.log("SubtypeController.add()", subtype);
+      });
+    };
+    
+    /*
+     $scope.delete = function(id){
+     console.log('delete(): ', $scope.subtypes[id], id);
+     $http.delete('api/subtype/'+id).then(function(resp){
+     console.log('Success', resp.data);
+     delete $scope.subtypes[id];
+     });
+     };
+     */
 
-  $scope.delete = function(id){
-    console.log('delete(): ', $scope.subtypes[id], id);
-    $http.delete('api/subtype/'+id).then(function(resp){
-      console.log('Success', resp.data);
-      delete $scope.subtypes[id];
-    });
-  };
-  */
-
-});
+  });
