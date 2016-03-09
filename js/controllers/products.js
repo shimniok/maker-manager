f0f5504fb9143eaca4e68a1ea8dd4632d48a0f5e
shimniok@gmail.com <shimniok@gmail.com>
2016-03-09 08:22:07 -0700

    Added method for add(), wired to resource and data service

--- js/controllers/products.js
+++ js/controllers/products.js
@@ -2,6 +2,16 @@ angular.module('MakerMRP')
   //////////////////////////////////////////////////////////////////////
   // PRODUCT Controller
   //
-  .controller('ProductController', function(DataService, $scope){
+  .controller('ProductController', function(DataService, ProductsResource, $scope){
     $scope.data = DataService;
+    $scope.new = new ProductsResource();
+    
+    $scope.add = function() {
+      console.log("ProductController.add()");
+      $scope.new.$save(function(product) {
+        DataService.products.push(product);
+        $scope.new = new ProductsResource();
+        console.log("ProductController.add()", product);
+      });
+    };
   });
