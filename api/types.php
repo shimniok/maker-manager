45d60af90dad043ba72365e13e6df133431a3937
shimniok@gmail.com <shimniok@gmail.com>
2016-03-08 20:44:39 -0700

    Fixed post (adding new), removed unnecessary use statements. Return entire added object.

--- api/types.php
+++ api/types.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * REST interface
  *
@@ -10,41 +11,43 @@
  * DELETE BASE_URL/thingy/12 - Deletes thingy #12
  */
 include_once 'base.php';
-require_once __DIR__.'/silex/vendor/autoload.php';
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+require_once __DIR__ . '/silex/vendor/autoload.php';
 
-$app = new Silex\Application();
+use Symfony\Component\HttpFoundation\Request;
+use Silex\Application;
+
+$app = new Application();
 
 $app['debug'] = true;
 
-$app->before(function (Symfony\Component\HttpFoundation\Request $request) {
-  if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-    $data = json_decode($request->getContent(), true);
-    $request->request->replace(is_array($data) ? $data : array());
-  }
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
 });
 
 $columns = array('id', 'name');
 $db = new Data($db, 'types', 'id', $columns);
 
-$app->get('/types', function() use($db, $columns) {
-  return json_encode($db->query());
+$app->get('/types', function() use($db) {
+    return json_encode($db->query());
 });
 
-$app->get('/types/{id}', function($id) use ($db, $columns) {
-  return json_encode($db->query('id', $id, 1));
+$app->get('/types/{id}', function($id) use ($db) {
+    return json_encode($db->query('id', $id, 1));
 });
 
-$app->post('/types', function(Silex\Application $app, Symfony\Component\HttpFoundation\Request $request) use($db) {
-  $data = $request->request->all();
-  $id = $db->add($data);
-  $data['id'] = "$id";
-  return json_encode($data);
+$app->post('/types', function(Request $request) use($db) {
+    $new = array(
+        "name" => $request->get("name")
+    );
+    return json_encode($db->add($new));
 });
 
-$app->delete('/types/{id}', function($id) use($db){
-  return json_encode($db->del($id));
+$app->delete('/types/{id}', function($id) use($db) {
+    $result = array('status' => $db->del($id));
+    return json_encode($result);
 });
 
 $app->run();
